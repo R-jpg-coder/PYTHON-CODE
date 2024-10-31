ce8e552d6be75b2bbf42a4724a41b2f89eed401b
R-jpg-coder <72785465+R-jpg-coder@users.noreply.github.com>
2024-10-31 01:34:51 -0400

    Add files via upload

---
+++ Untitled Document 18.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("GeeksforGeeks"),
+        ), // AppBar
+        body: const Center(
+          child: Text("Hello Geeks!!"),
+        ), // Container
+      ), // Scaffold
+    );// MaterialApp
+  }
+}
+
+
